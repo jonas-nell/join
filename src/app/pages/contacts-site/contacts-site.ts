7d665c2c492c1abd7053b00b9dd6625240b18282
DanielGengel <daniel.gengel@outlook.com>
2026-08-11 19:21:48 +0200

    Feat: Database and user-list-view

--- src/app/pages/contacts-site/contacts-site.ts
+++ src/app/pages/contacts-site/contacts-site.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { UserList } from '../../shared/components/user-list/user-list';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-contacts-site',
-  imports: [],
+  imports: [RouterOutlet, UserList],
   templateUrl: './contacts-site.html',
   styleUrl: './contacts-site.scss',
 })
-export class ContactsSite {}
+export class ContactsSite {
+  // True when a profile route is open.
+  readonly profileOpen = signal(false);
+}
