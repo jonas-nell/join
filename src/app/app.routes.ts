17d3f3b1512cd0a6a67dafff1647b7a356c1470a
Jonas Nell <jonasnell1705@gmail.com>
2026-08-12 00:37:44 +0200

    feat: added legal notice, privacy policy, help pages; added routing legal, privp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { Help } from './pages/help/help';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'legal-notice', component: LegalNotice },
+    { path: 'privacy-policy', component: PrivacyPolicy },
+    { path: 'help', component: Help },
+];
