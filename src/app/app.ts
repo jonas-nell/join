a812ae6d51823a7699c3e90bee9274e9b0a58a3c
Jonas Nell <jonasnell1705@gmail.com>
2026-08-12 13:41:26 +0200

    fix: routerlinkactive contacts site

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContactsSite } from "./pages/contacts-site/contacts-site";
 import { NavBar } from "./layout/nav-bar/nav-bar";
 
 @Component({
