ea5ab90377a7d4d3230dcc493ef3aad8ac92c670
LeaFriedmann <friedmann.lea.dev@gmail.com>
2026-08-13 11:58:29 +0200

    feat: header menu animation

--- src/app/layout/header/nav-bar/header-menu.ts
+++ src/app/layout/header/nav-bar/header-menu.ts
@@ -1,40 +1,14 @@
 import { Component, effect, ElementRef, inject, input, viewChild } from '@angular/core';
 import { DialogService } from '../../../dialog-service';
-import { Dialog } from "../../../dialog-directive";
+import { Dialog } from '../../../dialog-directive';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-header-menu',
-  imports: [Dialog, RouterLink, RouterLinkActive],
-  templateUrl: './header-menu.html',
-  styleUrl: './header-menu.scss',
+    selector: 'app-header-menu',
+    imports: [Dialog, RouterLink, RouterLinkActive],
+    templateUrl: './header-menu.html',
+    styleUrl: './header-menu.scss',
 })
 export class HeaderMenu {
-  // dialogService = inject(DialogService);
-
-  // dialog = viewChild<ElementRef<HTMLDialogElement>>('dialog');
-
-  // constructor() {
-  //   effect(() => {
-  //     const dialog = this.dialog()?.nativeElement;
-
-  //     if (!dialog) {
-  //       return;
-  //     }
-
-  //     if (this.dialogService.dialogOpen() == 'menu') {
-  //       dialog.showModal();
-  //     }
-
-  //     if (this.dialogService.dialogOpen() != 'menu') {
-  //       dialog.close();
-  //     }
-  //   });
-  // }
-
-  // onDialogClick(event: MouseEvent) {
-  //   if (event.target === event.currentTarget) {
-  //     this.dialogService.closeDialog();
-  //   }
-  // }
+    dialogservice = inject(DialogService);
 }
