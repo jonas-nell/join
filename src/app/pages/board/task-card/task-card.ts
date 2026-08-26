c16a374d555642193464799d9ddc95a55473a2c2
Jonas Nell <jonasnell1705@gmail.com>
2026-08-26 11:29:46 +0200

    feat: implemented category badge

--- src/app/pages/board/task-card/task-card.ts
+++ src/app/pages/board/task-card/task-card.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { CdkDrag, CdkDragPlaceholder } from '@angular/cdk/drag-drop';
 import { Task } from '../../../shared/interfaces/task';
+import { CategoryBadge } from "../../../shared/category-badge/category-badge";
 
 @Component({
     selector: 'app-task-card',
-    imports: [CdkDrag, CdkDragPlaceholder],
+    imports: [CdkDrag, CdkDragPlaceholder, CategoryBadge],
     templateUrl: './task-card.html',
     styleUrl: './task-card.scss',
 })
