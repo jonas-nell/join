1f10e74aef9890fd8d97c3064caf62e8d2c2558e
DanielGengel <daniel.gengel@outlook.com>
2026-08-31 09:46:14 +0200

    Feat: Click outside dialog shows confirmation on changes

--- src/app/shared/directives/dialog-directive.ts
+++ src/app/shared/directives/dialog-directive.ts
@@ -2,54 +2,64 @@ import { Directive, effect, ElementRef, HostListener, inject, input } from '@ang
 import { DialogName, DialogService } from '../services/dialog-service';
 
 @Directive({
-  // directive wird nur auf dialog elemente angewendet (mit dem attribut appDialog)
-  selector: 'dialog[appDialog]',
-  host: {
-    '(click)': 'onClickClose($event)',
-  },
+    // directive wird nur auf dialog elemente angewendet (mit dem attribut appDialog)
+    selector: 'dialog[appDialog]',
+    host: {
+        '(click)': 'onClickClose($event)',
+    },
 })
 export class Dialog {
-  dialogService = inject(DialogService);
-  dialog = inject(ElementRef<HTMLDialogElement>);
+    dialogService = inject(DialogService);
+    dialog = inject(ElementRef<HTMLDialogElement>);
 
-  // input gleichen namen wie directive selector geben, damit directive angewendet und gleichzeitig ein wert übergeben wird
-  appDialog = input.required<DialogName>();
-  modal = input<boolean>(true);
+    // input gleichen namen wie directive selector geben, damit directive angewendet und gleichzeitig ein wert übergeben wird
+    appDialog = input.required<DialogName>();
+    modal = input<boolean>(true);
 
-  constructor() {
+    // DAniel
+    closeOnBackdrop = input<boolean>(true);
 
-    // wird ausgeführt wenn sich der wert von dialogOpen() im service verändert
-    effect(() => {
+    constructor() {
+        // wird ausgeführt wenn sich der wert von dialogOpen() im service verändert
+        effect(() => {
+            // nativeElement = referenz zu host Element instanz
+            const dialog = this.dialog.nativeElement;
+            const name = this.appDialog();
 
-      // nativeElement = referenz zu host Element instanz
-      const dialog = this.dialog.nativeElement;
-      const name = this.appDialog();
-
-      if (this.dialogService.dialogOpen() == name) {
-        this.modal() ? dialog.showModal() : dialog.show();
-      } else {
-        dialog.close();
-      }
-    });
-  }
-
-  // dialog wird beim klick auf das element geschlossen. dazu zählt auch der backdrop
-  // klick auf elemente im dialog schließen ihn nicht. daher darf der dialog kein padding haben, sondern nur die elemente darin
-  // (falls nötig wrapper innerhalb von dialog nutzen)
-  onClickClose(event: MouseEvent) {
-    if (event.target === event.currentTarget) {
-      this.dialogService.closeDialog();
+            if (this.dialogService.dialogOpen() == name) {
+                this.modal() ? dialog.showModal() : dialog.show();
+            } else {
+                dialog.close();
+            }
+        });
     }
-  }
 
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: MouseEvent) {
-    if (this.modal()) return;
-    if (!this.dialog.nativeElement.open) return;
+    // dialog wird beim klick auf das element geschlossen. dazu zählt auch der backdrop
+    // klick auf elemente im dialog schließen ihn nicht. daher darf der dialog kein padding haben, sondern nur die elemente darin
+    // (falls nötig wrapper innerhalb von dialog nutzen)
 
-    const target = event.target as Node;
-    if (!this.dialog.nativeElement.contains(target)) {
-      this.dialogService.closeDialog();
+    // Daniel
+
+    // onClickClose(event: MouseEvent) {
+    //   if (event.target === event.currentTarget) {
+    //     this.dialogService.closeDialog();
+    //   }
+    // }
+
+    onClickClose(event: MouseEvent): void {
+        if (event.target === event.currentTarget && this.closeOnBackdrop()) {
+            this.dialogService.closeDialog();
+        }
     }
-  }
+
+    @HostListener('document:click', ['$event'])
+    onDocumentClick(event: MouseEvent) {
+        if (this.modal()) return;
+        if (!this.dialog.nativeElement.open) return;
+
+        const target = event.target as Node;
+        if (!this.dialog.nativeElement.contains(target)) {
+            this.dialogService.closeDialog();
+        }
+    }
 }
