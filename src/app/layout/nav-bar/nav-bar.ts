2039cd2eeb38a605fb7494fcc7bad9368f35eb2f
LeaFriedmann <friedmann.lea.dev@gmail.com>
2026-09-01 20:10:39 +0200

    feat: edit form, form subrsribtions

--- src/app/layout/nav-bar/nav-bar.ts
+++ src/app/layout/nav-bar/nav-bar.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { filter, map } from 'rxjs';
+import { Taskmanagement } from '../../shared/services/taskmanagement';
 
 @Component({
     selector: 'app-nav-bar',
@@ -11,6 +12,7 @@ import { filter, map } from 'rxjs';
 })
 export class NavBar {
     private router = inject(Router);
+    taskmanagement = inject(Taskmanagement);
 
     isBoardPage = toSignal(
         this.router.events.pipe(
