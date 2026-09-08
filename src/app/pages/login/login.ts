d89655761d7bca52e26eaf7663f9b4223c9638b1
DanielGengel <daniel.gengel@outlook.com>
2026-09-08 19:23:35 +0200

    Fix: Imports of forms added

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginForm } from "./login-form/login-form";
+import { SignupForm } from "./signup-form/signup-form";
 
 @Component({
     selector: 'app-login',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, LoginForm, SignupForm],
     templateUrl: './login.html',
     styleUrl: './login.scss',
 })
